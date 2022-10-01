d8e972dfd80b4c4adc1d2cc159514416f57432b5
Neko ♥ <keonstraten@gmail.com>
2022-10-01 23:45:32 +0200

    General text shadowing. Added classnames for .css Removed css from bulma-overrides

--- src/pages/setupPhase/Intro.tsx
+++ src/pages/setupPhase/Intro.tsx
@@ -1,29 +1,32 @@
 import Layout from "../../Layout"
 import * as React from "react"
 import { Phase, SetupCommonProps } from "../FirstTimeSetupPage"
+import "./../../styles/intro.css"
 
 export default function Intro(props: SetupCommonProps) {
     return (
         <Layout>
             <section className="hero-body">
-                <h1 className="title">Peacock Launcher</h1>
+                <h1 className="title launcher-text">Peacock Launcher</h1>
 
-                {IS_DEVELOPMENT && (
-                    <button onClick={() => props.setPhase(Phase.Testing)}>
-                        Secret testing page
-                    </button>
-                )}
+                {/*{IS_DEVELOPMENT && (*/}
+                {/*    <button onClick={() => props.setPhase(Phase.Testing)}>*/}
+                {/*        Secret testing page*/}
+                {/*    </button>*/}
+                {/*)}*/}
 
                 <div className="container middle-child has-text-centered">
                     <div>
-                        <p className="title">Welcome, let's get started!</p>
+                        <p className="title welcome-text">
+                            Welcome, let's get started!
+                        </p>
                     </div>
                 </div>
 
                 <div className="container has-text-centered">
                     <button
                         type="button"
-                        className="button is-primary"
+                        className="button is-primary begin-button"
                         onClick={() => props.setPhase(Phase.PickGameVersion)}
                     >
                         Begin Setup
