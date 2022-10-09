4386b090bcd8b1592138801eb09701bfc51c41a4
Neko ♥ <keonstraten@gmail.com>
2022-10-09 04:05:20 +0200

    gameVersion styling

--- src/pages/setupPhase/PickGameVersion.tsx
+++ src/pages/setupPhase/PickGameVersion.tsx
@@ -1,18 +1,21 @@
 import { SetupCommonProps } from "../FirstTimeSetupPage"
 import Layout from "../../Layout"
 import * as React from "react"
+import "../../styles/gameVersion.css"
 import SelectGameVersion from "../../components/SelectGameVersion"
 
 export default function PickGameVersion(props: SetupCommonProps) {
     return (
         <Layout>
-            <h1 className="text-white p-4 text-center text-4xl">
+            <h1 className="text-white p-4 text-center text-4xl launcher-text">
                 Peacock Launcher
             </h1>
 
-            <SelectGameVersion />
+            <div className="game-version">
+                <SelectGameVersion />
+            </div>
 
-            <div className="text-center">
+            <div className="text-center next-button-wrapper">
                 <button
                     type="button"
                     className="rounded bg-green-600 text-white pt-1 pb-1 pl-2 pr-2"
